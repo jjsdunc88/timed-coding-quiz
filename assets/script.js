a4df26768c995bee0363bb04c1744f15fad41e7d
JDunc <jjsduncan@gmail.com>
2023-04-05 19:35:12 -0400

    created an array of objects in js file

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,28 @@
 console.log("Howdy");
+
+var quizBoxes = [
+    {
+        question1: "q1answer"
+    },
+
+    {
+        question2: "q2answer"
+    },
+
+    {
+        question3: "q3answer"
+    },
+
+    {
+        question4: "q4answer"
+    },
+
+    {
+        question5: "q5answer"
+    },
+];
+
+for(var i = 0; i < quizBoxes.length; i++){
+    console.log(quizBoxes[i]);
+    //will create boxes after class
+}
