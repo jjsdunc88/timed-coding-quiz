686aea10871632f0994bc8e4335dc4373273a852
JDunc <jjsduncan@gmail.com>
2023-04-05 20:25:51 -0400

    added statements to js

--- assets/script.js
+++ assets/script.js
@@ -4,27 +4,49 @@ var boxElem = document.querySelector("#color-flip-box");
 
 var quizBoxes = [
     {
-        question1: "q1answer"
+        name: "q1answer"
     },
 
     {
-        question2: "q2answer"
+        name: "q2answer"
     },
 
     {
-        question3: "q3answer"
+        name: "q3answer"
     },
 
     {
-        question4: "q4answer"
+        name: "q4answer"
     },
 
     {
-        question5: "q5answer"
+        name: "q5answer"
     },
 ];
 
-for(var i = 0; i < quizBoxes.length; i++){
+for (var i = 0; i < quizBoxes.length; i++) {
     console.log(quizBoxes[i]);
     //will create boxes after class
 }
+
+boxElem.style.backgroundColor = "green";
+var boxIsGreen = true;
+var boxIndex = 0;
+
+// on every click
+boxElem.addEventListener("click", function () {
+    // if box is green
+    if (boxIsGreen) {
+        boxElem.style.backgroundColor = "yellow";
+        boxIsGreen = false;
+    }
+    else{
+        boxElem.style.backgroundColor = "green";
+        boxIsGreen = true;
+    }
+    
+    console.log(quizBoxes[boxIndex].name);
+    boxIndex++;
+    if(boxIndex >= quizBoxes.length)
+        boxIndex = 0;
+});
